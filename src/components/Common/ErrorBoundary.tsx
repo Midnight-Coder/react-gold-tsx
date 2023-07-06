c7c73141ae32726dbd84963f8e01bef73c7fcd85
Midnight-Coder <sagar.jhobalia@gmail.com>
2023-07-05 18:51:26 -0700

    adds error codes to error boundary
    Signed-off-by: Midnight-Coder <sagar.jhobalia@gmail.com>

--- src/components/Common/ErrorBoundary.tsx
+++ src/components/Common/ErrorBoundary.tsx
@@ -7,7 +7,7 @@ import exceptionImg from 'assets/error-500.png';
 import BrandingBorder from 'components/Layouts/BrandingBorder';
 import React, { ErrorInfo, ReactNode } from 'react';
 import { Link } from 'react-router-dom';
-import { helpEmail } from 'utils/errorUtils';
+import { ErrorCodes, helpEmail } from 'utils/errorUtils';
 import { Logger } from 'utils/logger';
 import { homepage } from 'utils/spaUrls';
 
@@ -32,7 +32,7 @@ class ErrorBoundary extends React.Component <IProps, IState> {
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    Logger.error(`${error}, ${errorInfo.componentStack}`);
+    Logger.error(ErrorCodes.common.uncaughtExpection, `${error}, ${errorInfo.componentStack}`);
   }
 
   render() {
