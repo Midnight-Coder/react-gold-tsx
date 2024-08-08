8053c07f4d4516732552ac709172d671d29d1e05
Midnight-Coder <sagar.jhobalia@gmail.com>
2024-08-07 18:06:45 -0700

    updates to node v20.0.0
    Signed-off-by: Midnight-Coder <sagar.jhobalia@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Router from 'Router';
 import AppAlerts from 'components/Common/AppAlerts';
 import ErrorBoundary from 'components/Common/ErrorBoundary';
@@ -11,8 +10,6 @@ import { BrowserRouter } from 'react-router-dom';
 import Theme from 'theme';
 import { CacheKeys } from 'utils/constants';
 
-
-const queryClient = new QueryClient();
 
 export default function App() {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
@@ -28,18 +25,16 @@ export default function App() {
   return (
     <React.StrictMode>
       <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <Auth0ProviderWithHistory>
-            <ThemeProvider theme={Theme}>
-              <CssBaseline enableColorScheme />
-              <AppAlerts>
-                <ErrorBoundary>
-                  <Router />
-                </ErrorBoundary>
-              </AppAlerts>
-            </ThemeProvider>
-          </Auth0ProviderWithHistory>
-        </QueryClientProvider>
+        <Auth0ProviderWithHistory>
+          <ThemeProvider theme={Theme}>
+            <CssBaseline enableColorScheme />
+            <AppAlerts>
+              <ErrorBoundary>
+                <Router />
+              </ErrorBoundary>
+            </AppAlerts>
+          </ThemeProvider>
+        </Auth0ProviderWithHistory>
       </BrowserRouter>
     </React.StrictMode>
   );
